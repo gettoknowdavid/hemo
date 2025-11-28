8ef917a682e02181844264ea37c558aab89b33e6
David Michael II <knowdavidmichael@gmail.com>
2025-11-28 01:41:05 +0100

    feat: Add personal information scope
    This commit introduces a new `personalInfo` scope to the `HScope` constants.
    
    This new scope will be used to manage access related to a user's personal information. The `basicInfo` scope has been commented out, indicating it is deprecated or replaced by `personalInfo`.

--- lib/_shared/services/models/h_scope.dart
+++ lib/_shared/services/models/h_scope.dart
@@ -6,5 +6,7 @@ final class HScope {
   static const unauthenticated = 'UNAUTHENTICATED';
   static const unverified = 'UNVERIFIED';
   static const noPhoneNumber = 'NO_PHONE_NUMBER';
+  static const personalInfo = 'PERSONAL_INFORMATION';
+  // static const basicInfo = 'BASIC_INFORMATION';
   static const authenticated = 'AUTHENTICATED';
 }
